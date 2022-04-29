913cb82f1e95aa9767e1f52903e78c3b629e35de
Peter Szabo <peter.szabo@agoda.com>
2022-04-29 15:53:32 +0700

    Refactor for testability

--- src/lib.rs
+++ src/lib.rs
@@ -1,17 +1,17 @@
 use actix_web::{web, App, HttpResponse, HttpServer};
+use actix_web::dev::Server;
 async fn health_check() -> HttpResponse { 
-    HttpResponse::Ok()
+    HttpResponse::Ok().finish()
 }
-
-// We need to mark `run` as public.
-// It is no longer a binary entrypoint, therefore we can mark it as async 
-// without having to use any proc-macro incantation.
-pub async fn run() -> std::io::Result<()> {
-    HttpServer::new(|| { 
+// Notice the different signature!
+// We return `Server` on the happy path and we dropped the `async` keyword // We have no .await call, so it is not needed anymore.
+pub fn run() -> std::io::Result<Server> {
+    let server = HttpServer::new(|| { 
         App::new()
             .route("/health_check", web::get().to(health_check))
     }) 
     .bind("127.0.0.1:8000")? 
-    .run()
-    .await
+    .run();
+    
+    Ok(server)
 }
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 use zero2prod::run;
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
-    run().await
+    run()?.await
 }
