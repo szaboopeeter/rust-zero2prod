d9f83fe06223d60e079c398d61a51cd327f92b1d
Peter Szabo <peter.szabo@agoda.com>
2022-10-30 15:55:46 +0700

    Add default behavior for subscribe endpoint

--- src/lib.rs
+++ src/lib.rs
@@ -6,12 +6,17 @@ async fn health_check() -> HttpResponse {
     HttpResponse::Ok().finish()
 }
 
+async fn subscribe() -> HttpResponse {
+    HttpResponse::Ok().finish()
+}
+
 // Notice the different signature!
 // We return `Server` on the happy path and we dropped the `async` keyword // We have no .await call, so it is not needed anymore.
 pub fn run(listener: TcpListener) -> Result<Server, std::io::Error> {
     let server = HttpServer::new(|| { 
         App::new()
             .route("/health_check", web::get().to(health_check))
+            .route("/subscriptions", web::post().to(subscribe))
     }) 
     .listen(listener)? 
     .run();
