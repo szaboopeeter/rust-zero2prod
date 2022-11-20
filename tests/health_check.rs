6c5e6941e0b367ca2a1e8fd4967247f15915947b
Peter Szabo <peter.szabo@agoda.com>
2022-11-20 18:09:50 +0700

    Isolate test database between runs

--- tests/health_check.rs
+++ tests/health_check.rs
@@ -1,6 +1,7 @@
 use std::net::TcpListener;
-use sqlx::PgPool;
-use zero2prod::configuration::get_configuration;
+use sqlx::{PgPool, PgConnection, Connection, Executor};
+use uuid::Uuid;
+use zero2prod::configuration::{get_configuration, DatabaseSettings};
 
 pub struct TestApp {
     pub address: String,
@@ -28,13 +29,12 @@ async fn spawn_app() -> TestApp {
     let port = listener.local_addr().unwrap().port();
     let address = format!("http://127.0.0.1:{}", port);
 
-    let configuration = get_configuration().expect("Failed to read configuration.");
-    let connection_pool = PgPool::connect(&configuration.database.connection_string())
-        .await
-        .expect("Failed to connecto to Postgres.");
+    let mut configuration = get_configuration().expect("Failed to read configuration.");
+    configuration.database.database_name = Uuid::new_v4().to_string();
+    let connection_pool = configure_database(&configuration.database).await;
 
     let server = zero2prod::startup::run(listener, connection_pool.clone())
-        .expect("failed to bind address");
+        .expect("Failed to bind address.");
     let _ = tokio::spawn(server);
     TestApp {
         address,
@@ -42,12 +42,33 @@ async fn spawn_app() -> TestApp {
     }
 }
 
+pub async fn configure_database(config: &DatabaseSettings) -> PgPool {
+    // create db
+    let mut connection = PgConnection::connect(&config.connection_string_without_db())
+        .await
+        .expect("Failed to connect to Postgres.");
+    connection.execute(format!(r#"CREATE DATABASE "{}";"#, config.database_name).as_str())
+        .await
+        .expect("Failed to create database.");
+
+    // Migrate db
+    let connection_pool = PgPool::connect(&config.connection_string())
+        .await
+        .expect("Failed to connect to Postgres.");
+    sqlx::migrate!("./migrations")
+        .run(&connection_pool)
+        .await
+        .expect("Failed to migrate database.");
+    
+    connection_pool
+}
+
 #[tokio::test]
 async fn subscribe_returns_a_200_for_valid_form_data() {
     let test_app = spawn_app().await;
     let client = reqwest::Client::new();
 
-    let body = "name=le%20guin&email=ursula_le_guin%example.com";
+    let body = "name=le%20guin&email=ursula_le_guin@example.com";
     let response = client
         .post(&format!("{}/subscriptions", &test_app.address))
         .header("Content-Type", "application/x-www-form-urlencoded")
