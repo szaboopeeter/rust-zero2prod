2e7018dface5700b43b4f490cdab3cbb071a9d03
Peter Szabo <peter.szabo@agoda.com>
2022-11-27 19:06:46 +0700

    Poor-man's correlation id

--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -9,7 +9,8 @@ pub async fn subscribe(
     form: web::Form<FormData>,
     pool: web::Data<PgPool>
 ) -> HttpResponse {
-    log::info!("Saving new subscriber details in the database");
+    let request_id = Uuid::new_v4();
+    log::info!("request_id {} - Saving new subscriber details in the database", request_id);
     // `Result` has two variants: `Ok` and `Err`.
     // The first for successes, the second for failures.
     // We use a `match` statement to choose what to do based 
@@ -28,11 +29,11 @@ pub async fn subscribe(
     .await 
     {
         Ok(_) =>{ 
-            log::info!("Adding '{}' '{}' as a new subscriber", form.email, form.name);
+            log::info!("request_id {} - Adding '{}' '{}' as a new subscriber", request_id, form.email, form.name);
             HttpResponse::Ok().finish()
         },
         Err(e) => {
-            log::error!("Failed to execute query: {:?}", e);
+            log::error!("request_id {} - Failed to execute query: {:?}", request_id, e);
             HttpResponse::InternalServerError().finish()
         }
     }
