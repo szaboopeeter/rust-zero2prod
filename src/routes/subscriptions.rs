798809c6131733c5499db78f60a269b6e3d6f591
Peter Szabo <peter.szabo@agoda.com>
2022-11-27 19:31:43 +0700

    Tracing&spans

--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -10,7 +10,18 @@ pub async fn subscribe(
     pool: web::Data<PgPool>
 ) -> HttpResponse {
     let request_id = Uuid::new_v4();
-    log::info!("request_id {} - Saving new subscriber details in the database", request_id);
+    let request_span = tracing::info_span!(
+        "Saving new subscriber details in the database", 
+        %request_id,
+        subscriber_email = %form.email,
+        subscriber_name = %form.name
+    );
+    // TODO: Should not use `enter()` inside async functions
+    // As long as the guard variable is not dropped, all downstream spans
+    // will be the children of the entered span
+    // RAII pattern: https://doc.rust-lang.org/stable/rust-by-example/scope/raii.html
+    let _request_span_guard = request_span.enter();
+
     // `Result` has two variants: `Ok` and `Err`.
     // The first for successes, the second for failures.
     // We use a `match` statement to choose what to do based 
@@ -29,11 +40,11 @@ pub async fn subscribe(
     .await 
     {
         Ok(_) =>{ 
-            log::info!("request_id {} - Adding '{}' '{}' as a new subscriber", request_id, form.email, form.name);
+            tracing::info!("request_id {} - Adding '{}' '{}' as a new subscriber", request_id, form.email, form.name);
             HttpResponse::Ok().finish()
         },
         Err(e) => {
-            log::error!("request_id {} - Failed to execute query: {:?}", request_id, e);
+            tracing::error!("request_id {} - Failed to execute query: {:?}", request_id, e);
             HttpResponse::InternalServerError().finish()
         }
     }
